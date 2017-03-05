1f7b1cf98c7c4b999c69e7985e7d335b96965145
Definechi <yuxuanryanwang@gmail.com>
2017-03-05 15:35:43 -0500

    Add files via upload

---
+++ It's not rocket science/fire fightinh.py
@@ -0,0 +1,33 @@
+from gopigo import*
+from time import sleep
+fwd()
+sleep(4.8)
+stop()
+left()
+sleep(0.5)
+stop()
+fwd()
+sleep(1.8)
+stop()      
+left()
+sleep(.4)
+stop()
+fwd()
+sleep(.1)
+stop()
+fwd()
+sleep(.1)
+stop()
+fwd()
+sleep(.7)
+stop()
+right()
+sleep(.5)
+stop()
+fwd()
+sleep(.1)
+stop()
+
+
+
+    
