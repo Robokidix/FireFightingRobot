3fb5367db833d88cf834e35d931d01725422007c
ninjapotatoes <2022theodorez507@gmail.com>
2017-03-19 13:19:23 -0400

    Add files via upload

---
+++ It's not rocket science/teddytest2.0.py
@@ -0,0 +1,10 @@
+from gopigo import*
+increase_speed()
+increase_speed()
+increase_speed()
+increase_speed()
+increase_speed()
+fwd()
+time.sleep(10)
+stop()
+
---
+++ It's not rocket science/teddytest2.py
@@ -0,0 +1,4 @@
+Python 3.4.2 (default, Oct 19 2014, 13:31:11) 
+[GCC 4.9.1] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
